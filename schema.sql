79fb4ec7ceb9d779185c14e7f4bba0751a1356cd
fireworks88 <throwaway.546372819@protonmail.com>
2022-01-31 23:33:30 +0100

    piss

--- schema.sql
+++ schema.sql
@@ -2,10 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.6
--- Dumped by pg_dump version 12.6
-
--- Started on 2021-10-22 17:08:35 CEST
+-- Dumped from database version 13.5
+-- Dumped by pg_dump version 13.5
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,7 +21,49 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 203 (class 1259 OID 28476)
+-- Name: posts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    title character varying(50),
+    body character varying(10000),
+    body_html text,
+    created_utc integer,
+    creation_ip character varying(255),
+    author_id integer,
+    board_id integer,
+    parent_id integer,
+    anon boolean DEFAULT true,
+    spam boolean DEFAULT false,
+    archived boolean DEFAULT false,
+    last_bump_utc integer,
+    attachment_url character varying(255),
+    quoted_by integer[] DEFAULT '{}'::integer[],
+    attachment_size integer DEFAULT 0,
+    attachment_type character varying(20) DEFAULT ''::character varying,
+    attachment_name character varying(255) DEFAULT ''::character varying,
+    attachment_mimetype character varying(255) DEFAULT ''::character varying,
+    mod boolean DEFAULT false,
+    pinned boolean DEFAULT false,
+    approved boolean DEFAULT false
+);
+
+
+--
+-- Name: comment_count(public.posts); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.comment_count(public.posts) RETURNS bigint
+    LANGUAGE sql IMMUTABLE STRICT
+    AS $_$
+SELECT COUNT(*)
+FROM posts
+WHERE parent_id=$1.id
+$_$;
+
+
+--
 -- Name: boards; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -41,7 +81,6 @@ CREATE TABLE public.boards (
 
 
 --
--- TOC entry 202 (class 1259 OID 28474)
 -- Name: boards_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -55,8 +94,6 @@ CREATE SEQUENCE public.boards_id_seq
 
 
 --
--- TOC entry 3573 (class 0 OID 0)
--- Dependencies: 202
 -- Name: boards_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -64,7 +101,97 @@ ALTER SEQUENCE public.boards_id_seq OWNED BY public.boards.id;
 
 
 --
--- TOC entry 205 (class 1259 OID 29098)
+-- Name: mods; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.mods (
+    id integer NOT NULL,
+    board_id integer NOT NULL,
+    user_id integer NOT NULL,
+    created_utc integer,
+    perm_content boolean DEFAULT true,
+    perm_users boolean DEFAULT true,
+    perm_config boolean DEFAULT true,
+    perm_styling boolean DEFAULT true
+);
+
+
+--
+-- Name: mods_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.mods_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: mods_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.mods_id_seq OWNED BY public.mods.id;
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.posts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
+-- Name: reports; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.reports (
+    id integer NOT NULL,
+    post_id integer NOT NULL,
+    reason character varying(255),
+    user_id integer NOT NULL,
+    is_global boolean DEFAULT false,
+    created_utc integer,
+    creation_ip character varying(255)
+);
+
+
+--
+-- Name: reports_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.reports_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: reports_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.reports_id_seq OWNED BY public.reports.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -86,7 +213,6 @@ CREATE TABLE public.users (
 
 
 --
--- TOC entry 204 (class 1259 OID 29096)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -100,8 +226,6 @@ CREATE SEQUENCE public.users_id_seq
 
 
 --
--- TOC entry 3574 (class 0 OID 0)
--- Dependencies: 204
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -109,7 +233,6 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 3427 (class 2604 OID 28479)
 -- Name: boards id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -117,7 +240,27 @@ ALTER TABLE ONLY public.boards ALTER COLUMN id SET DEFAULT nextval('public.board
 
 
 --
--- TOC entry 3430 (class 2604 OID 29101)
+-- Name: mods id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.mods ALTER COLUMN id SET DEFAULT nextval('public.mods_id_seq'::regclass);
+
+
+--
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id_seq'::regclass);
+
+
+--
+-- Name: reports id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reports ALTER COLUMN id SET DEFAULT nextval('public.reports_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -125,7 +268,6 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 3439 (class 2606 OID 28485)
 -- Name: boards boards_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -134,7 +276,30 @@ ALTER TABLE ONLY public.boards
 
 
 --
--- TOC entry 3441 (class 2606 OID 29113)
+-- Name: mods mods_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.mods
+    ADD CONSTRAINT mods_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: reports reports_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reports
+    ADD CONSTRAINT reports_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -142,7 +307,61 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
--- Completed on 2021-10-22 17:08:36 CEST
+--
+-- Name: mods mods_board_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.mods
+    ADD CONSTRAINT mods_board_id_fkey FOREIGN KEY (board_id) REFERENCES public.boards(id);
+
+
+--
+-- Name: mods mods_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.mods
+    ADD CONSTRAINT mods_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: posts posts_author_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_author_id_fkey FOREIGN KEY (author_id) REFERENCES public.users(id);
+
+
+--
+-- Name: posts posts_board_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_board_id_fkey FOREIGN KEY (board_id) REFERENCES public.boards(id);
+
+
+--
+-- Name: posts posts_parent_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_parent_id_fkey FOREIGN KEY (parent_id) REFERENCES public.posts(id);
+
+
+--
+-- Name: reports reports_post_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reports
+    ADD CONSTRAINT reports_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.posts(id);
+
+
+--
+-- Name: reports reports_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reports
+    ADD CONSTRAINT reports_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
 
 --
 -- PostgreSQL database dump complete
